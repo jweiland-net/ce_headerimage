80caaaee60d504c4c9f7b38a0cdf0df3cfefaa67
crontis <dev@crontis.net>
2020-07-16 15:54:03 +0200

    [Task] Added php_cs config and fixed file headers

--- Classes/Controller/SliderController.php
+++ Classes/Controller/SliderController.php
@@ -1,19 +1,14 @@
 <?php
+
 declare(strict_types=1);
-namespace JWeiland\CeHeaderimage\Controller;
 
 /*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
+ * This file is part of the package jweiland/ce_headerimage.
  * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
+ * LICENSE file that was distributed with this source code.
  */
+
+namespace JWeiland\CeHeaderimage\Controller;
 
 use JWeiland\CeHeaderimage\Domain\Model\Slider;
 use JWeiland\CeHeaderimage\Domain\Repository\SliderRepository;
@@ -23,8 +18,6 @@ use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
 /**
  * Class SliderController
- *
- * @package JWeiland\CeHeaderimage\Controller
  */
 class SliderController extends ActionController
 {
@@ -39,7 +32,6 @@ class SliderController extends ActionController
      * injects sliderRepository
      *
      * @param SliderRepository $sliderRepository
-     * @return void
      */
     public function injectSliderRepository(SliderRepository $sliderRepository)
     {
@@ -48,8 +40,6 @@ class SliderController extends ActionController
 
     /**
      * action show
-     *
-     * @return void
      */
     public function showAction(): void
     {
